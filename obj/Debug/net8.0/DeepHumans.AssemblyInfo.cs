c62d7fc320b31963a4a2a32f6678db548f313923
abdullah firas fawzi al obaidi <abdullahl4gnd@gmail.com>
2025-11-13 13:23:41 +0300

    LVL 5.1

--- obj/Debug/net8.0/DeepHumans.AssemblyInfo.cs
+++ obj/Debug/net8.0/DeepHumans.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeepHumans")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe173160fdb1272b12c866b2ff7646312950e771")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae83c7e2b08cc673d5b1702487b1e9d63d434e72")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeepHumans")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeepHumans")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
